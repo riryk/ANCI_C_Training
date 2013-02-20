8dc5f2c3853d3945552cb2ff415b3dae875595a7
unknown <Ruslan_Iryk@EPUAKYIW0246.kyiv.epam.com>
2013-02-20 21:12:00 +0300

    Implemented install of hashtable and started investigation of reading and writing of files

--- src/main/main.c
+++ src/main/main.c
@@ -3,11 +3,33 @@
 /* ANCI C reading 38 */
 int main(int argc, char *argv[])
 { 
-	char* str = (char*)malloc(4*sizeof(char));  
+	//int c = getchar();
+	//FILE* _file = fopen("D:\\Run.cmd", "r");
 
-	MemorySet(str, 0, 4*sizeof(char));
+	
+	HANDLE _fileHandler = CreateFile(
+		               "D:\\Run.cmd",                      // name of the write
+                       GENERIC_WRITE,                      // open for writing
+                       0,                                  // do not share
+                       NULL,                               // default security
+                       CREATE_NEW | OPEN_EXISTING,         // create new file only
+                       FILE_ATTRIBUTE_NORMAL,              // normal file
+                       NULL);   
 
+	if (_fileHandler == INVALID_HANDLE_VALUE)
+	{
+
+	}
 
 	//memset(str, 0, 4*sizeof(char));
+}
+
+void DisplayError()
+{
+	LPVOID lpMsgBuf;
+    LPVOID lpDisplayBuf;
+    DWORD dw = GetLastError(); 
+    
+
 }
 
