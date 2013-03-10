2a7074fc19bbd45b17672759564b6a6565f0b864
Ruslan Iryk <rriryk@gmail.com>
2013-03-10 18:12:41 +0200

    Acomplished problem 3 and finished singleton example writing

--- src/UI/Singleton/Singleton.cpp
+++ src/UI/Singleton/Singleton.cpp
@@ -13,15 +13,26 @@
 #include <CommCtrl.h>
 /* For _beginthreadex */
 #include <process.h>  
-
+#include <strsafe.h>
+/* for SID management */
+#include <Sddl.h>    
 
 /* Main dialog */
 HWND     g_hDlg;
+
+/* Mutex, boundary and namespace used to detect previous running instance */
+HANDLE   g_hSingleton = NULL;
 HANDLE   g_hBoundary = NULL;
+HANDLE   g_hNamespace = NULL;
+
+/* Keep track whether or not the namespace was created or open for clean-up */
+BOOL     g_bNamespaceOpened = FALSE;
 
 /* Names of boundary and private namespace */
 PCTSTR   g_szBoundary = TEXT("3-Boundary");
 PCTSTR   g_szNamespace = TEXT("3-Namespace");
+
+#define DETAILS_CTRL GetDlgItem(g_hDlg, IDC_EDIT_DETAILS)
 
 #define chHANDLE_DLGMSG(hWnd, message, fn)                 \
    case (message): return (SetDlgMsgResult(hWnd, uMsg,     \
@@ -46,12 +57,13 @@ void AddText(PCTSTR pszFormat, ...)
    TCHAR sz[20 * 1024];
 
    Edit_GetText(DETAILS_CTRL, sz, _countof(sz));
+   /* Save char print */
    _vstprintf_s
    (
       _tcschr(sz, TEXT('\0')), 
-	  _countof(sz) - _tcslen(sz), 
+   	  _countof(sz) - _tcslen(sz), 
       pszFormat, 
-	  argList
+   	  argList
    );
    Edit_SetText(DETAILS_CTRL, sz);
 
@@ -66,6 +78,8 @@ void CheckInstances()
    DWORD cbSID = sizeof(localAdminSID); 
    BOOL CreateWellKnownSidResult;
    BOOL AddSIDToBoundaryResult;
+   BOOL ConvertStringResult;
+   DWORD dwLastError;
    /* Create the boundary descriptor 
     * A new boundary descriptor must have at least one security identifier (SID). 
 	* To add a SID to a boundary descriptor, use the AddSIDToBoundaryDescriptor function.
@@ -94,7 +108,8 @@ void CheckInstances()
 	   CreateWellKnownSid(
 	     WinBuiltinAdministratorsSid,
          NULL,
-         &cbSID);
+         &cbSID,
+		 NULL);
    /* If we cannot create SID, we show a message on the interface. */ 
    if (CreateWellKnownSidResult == FALSE) 
    {
@@ -151,11 +166,122 @@ void CheckInstances()
     * SecurityDescriptor[out] - A pointer to a variable that receives a pointer to the converted security descriptor. 
 	* 
 	*/
-   if (!ConvertStringSecurityDescriptorToSecurityDescriptor(
-      TEXT("D:(A;;GA;;;BA)"), 
-      SDDL_REVISION_1, &sa.lpSecurityDescriptor, NULL)) {
-      AddText(TEXT("Security Descriptor creation failed: %u\r\n"), GetLastError());
+   ConvertStringResult = 
+	  ConvertStringSecurityDescriptorToSecurityDescriptor(
+         TEXT("D:(A;;GA;;;BA)"),
+         SDDL_REVISION_1,
+         &sa.lpSecurityDescriptor,
+	     NULL);
+    /* If we cannot convert string sequrity descriptor,
+	 * we show a message on the interface. */ 
+   if (ConvertStringResult == FALSE) 
+   {
+      AddText(
+		  TEXT("Security Descriptor creation failed: %u\r\n"), 
+		  GetLastError());
       return;
+   }
+   /* Creates a private namespace. 
+    *
+    * lpPrivateNamespaceAttributes [in, optional] -
+	*   A pointer to a SECURITY_ATTRIBUTES structure 
+    *   that specifies the security attributes of the namespace object.
+	* 
+ 	* lpBoundaryDescriptor [in] -
+    *   A descriptor that defines how the namespace is to be isolated. 
+	*   The caller must be within this boundary. 
+	*   The CreateBoundaryDescriptor function creates a boundary descriptor.
+	* 
+	* lpAliasPrefix [in] -
+    *   The prefix for the namespace. To create an object in this namespace,
+	*   specify the object name as prefix\objectname.
+    *   The system supports multiple private namespaces with the same name, 
+	*   as long as they define different boundaries.
+	* 
+	* Return value
+    *   If the function succeeds, it returns a handle to the new namespace.
+    *   If the function fails, the return value is NULL. 
+	*   To get extended error information, call GetLastError. 
+    */
+   g_hNamespace = 
+	  CreatePrivateNamespace(
+	    &sa, 
+	    g_hBoundary, 
+	    g_szNamespace);
+   /* The memory for sa.lpSecurityDescriptor has been allocated by Convert...
+    * windows function. It up to us to free the memory.
+    * Don't forget to release memory for the security descriptor.
+    */
+   LocalFree(sa.lpSecurityDescriptor);
+   /* Check the private namespace creation result */
+   dwLastError = GetLastError();
+   /* If the function has returned null. That means that some error
+    * has happened. In this case we need to investigate a GetLastError()
+    */
+   if (g_hNamespace == NULL) 
+   {
+      /* Nothing to do if access is denied
+       * --> this code must run under a Local Administrator account
+	   */
+      if (dwLastError == ERROR_ACCESS_DENIED) 
+	  {
+         AddText(TEXT("Access denied when creating the namespace.\r\n"));
+         AddText(TEXT("   You must be running as Administrator.\r\n\r\n"));
+         return;
+	  }
+	  else 
+	  { 
+         /* If another instance has already created the namespace, 
+            we need to open it instead.  */
+         if (dwLastError == ERROR_ALREADY_EXISTS) 
+		 {
+            AddText(TEXT("CreatePrivateNamespace failed: %u\r\n"), dwLastError);
+			/* Try to open existed private namespace 
+			 * pBoundaryDescriptor [in] - A descriptor that defines how 
+			 * the namespace is to be isolated. The CreateBoundaryDescriptor function
+			 * creates a boundary descriptor.
+			 */
+            g_hNamespace = OpenPrivateNamespace(g_hBoundary, g_szNamespace);
+			/* If the function returns null, that means that some error has happened */
+			if (g_hNamespace == NULL) 
+			{
+               AddText(
+				   TEXT("   and OpenPrivateNamespace failed: %u\r\n"), 
+                   dwLastError);
+               return;
+            } 
+			else 
+			{
+               g_bNamespaceOpened = TRUE;
+               AddText(TEXT("   but OpenPrivateNamespace succeeded\r\n\r\n"));
+            }
+		 }
+	  }
+   }
+
+   /* Try to create the mutex object with a name 
+    * based on the private namespace 
+	*/
+   TCHAR szMutexName[64];
+   StringCchPrintf(
+	   szMutexName, 
+	   _countof(szMutexName), 
+	   TEXT("%s\\%s"), 
+       g_szNamespace, 
+	   TEXT("Singleton"));
+   /* Try to create mutex */
+   g_hSingleton = CreateMutex(NULL, FALSE, szMutexName);
+   if (GetLastError() == ERROR_ALREADY_EXISTS) 
+   {
+      /* There is already an instance of this Singleton object */
+      AddText(TEXT("Another instance of Singleton is running:\r\n"));
+      AddText(TEXT("--> Impossible to access application features.\r\n"));
+   } 
+   else  
+   {
+      /* First time the Singleton object is created */
+      AddText(TEXT("First instance of Singleton:\r\n"));
+      AddText(TEXT("--> Access application features now.\r\n"));
    }
 }
 
@@ -205,31 +331,31 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
    /* Show main window */
    DialogBox(hInstance, MAKEINTRESOURCE(IDD_SINGLETON), NULL, Dlg_Proc);
 
-   ///* Don't forget to clean up and release kernel resources */
-   //if (g_hSingleton != NULL) 
-   //{
-   //    CloseHandle(g_hSingleton);
-   //}
+   /* Don't forget to clean up and release kernel resources */
+   if (g_hSingleton != NULL) 
+   {
+       CloseHandle(g_hSingleton);
+   }
 
-   //if (g_hNamespace != NULL) 
-   //{
-	  // /* Open namespace */
-   //    if (g_bNamespaceOpened) 
-	  // {  
-		 // /* Open namespace */
-   //       ClosePrivateNamespace(g_hNamespace, 0);
-   //    } 
-	  // else 
-	  // {  
-		 // /* Created namespace */
-   //       ClosePrivateNamespace(g_hNamespace, PRIVATE_NAMESPACE_FLAG_DESTROY);
-   //    }
-   //}
+   if (g_hNamespace != NULL) 
+   {
+	   /* Open namespace */
+       if (g_bNamespaceOpened) 
+	   {  
+		   /* Open namespace */
+           ClosePrivateNamespace(g_hNamespace, 0);
+       } 
+	   else 
+	   {  
+		   /* Created namespace */
+           ClosePrivateNamespace(g_hNamespace, PRIVATE_NAMESPACE_FLAG_DESTROY);
+       }
+   }
 
-   //if (g_hBoundary != NULL) 
-   //{
-   //   DeleteBoundaryDescriptor(g_hBoundary);
-   //}
+   if (g_hBoundary != NULL) 
+   {
+       DeleteBoundaryDescriptor(g_hBoundary);
+   }
 
    return(0);
 }
--- src/mthread/mthread.c
+++ src/mthread/mthread.c
@@ -49,6 +49,51 @@ DWORD WaitFor(CONST HANDLE *threadHandlers, int threadCount)
 		TRUE,
 		INFINITE);
 }
-
-
+/* This test method illustrates how to pass over a handle to some 
+ * kernel object from one process to other process. This function 
+ * must be called in a source process.
+ * dwTProcessId - is an identifier of a target process identifier
+ * where we are going to copy a handle to.
+ */
+void DuplicateHandleToProcess(DWORD dwTProcessId)
+{
+	/* An uninitialized handle relative to process T */
+	HANDLE hObjInProcessT;
+	/* Some kernel object inside the process S. */
+    HANDLE hObjInProcessS = CreateMutex(NULL, FALSE, NULL);
+	/* Then we need to obtain a handle to a target process 
+	 * Opens an existing local process object.
+	 * dwDesiredAccess [in] - The access to the process object. 
+	 *    This access right is checked against the security descriptor for the process. 
+	 *    This parameter can be one or more of the process access rights.
+	 * bInheritHandle [in] - If this value is TRUE, processes created by this process 
+	 *    will inherit the handle. Otherwise, the processes do not inherit this handle.
+	 * dwProcessId [in] - The identifier of the local process to be opened.
+     *    If the specified process is the System Process (0x00000000), 
+	 *    the function fails and the last error code is ERROR_INVALID_PARAMETER. 
+	 *    If the specified process is the Idle process or one of the CSRSS processes, 
+	 *    this function fails and the last error code is ERROR_ACCESS_DENIED 
+	 *    because their access restrictions prevent user-level code from opening them.
+	 */
+	HANDLE processT = OpenProcess(PROCESS_ALL_ACCESS, FALSE, dwTProcessId);
+    /* This function duplicates the handle into the target process handle table
+	 * and returns a reference to this handle. And we need to use some kind of 
+	 * inter process communication to notify the target process about the fact
+	 * that the handle is now available for him.
+	 */
+	DuplicateHandle(
+		GetCurrentProcess(),
+        hObjInProcessS,
+        dwTProcessId,
+		&hObjInProcessT,
+		0,
+		FALSE,
+		DUPLICATE_SAME_ACCESS);
+	/* Use some kind or IPC mechanism to get the handle value of hObjInProcessS into 
+	 * process T. We no longer need to communicate with Process T.
+	 */
+    CloseHandle(processT);
+	/* When process S no longer needs to use the mutex, it should close it. */
+	CloseHandle(hObjInProcessS);
+}
 
