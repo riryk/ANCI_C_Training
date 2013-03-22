e4e3af62c135954dc35d2939dca3c05871cf712d
unknown <Ruslan_Iryk@EPUAKYIW0246.kyiv.epam.com>
2013-03-22 19:41:53 +0300

    Acomplished problem 5 and started solving problem 6

--- src/mthread/mthread.c
+++ src/mthread/mthread.c
@@ -392,6 +392,26 @@ int StartRestrictedProcess()
     */
     BOOL bInJob = FALSE;
 	HANDLE hjob;
+	/* Place some restrictions on processes in the job. 
+	 * First, set some basic restrictions.
+	 */
+	JOBOBJECT_BASIC_LIMIT_INFORMATION jobli = { 0 };
+	/* Second, set some UI restrictions. */
+	JOBOBJECT_BASIC_UI_RESTRICTIONS jobuir;
+	STARTUPINFO si = { sizeof(si) };
+	PROCESS_INFORMATION pi;
+    TCHAR szCmdLine[8];
+	BOOL bResult;
+
+	FILETIME CreationTime;
+    FILETIME ExitTime;
+    FILETIME KernelTime;
+    FILETIME UserTime;
+    TCHAR szInfo[MAX_PATH];
+
+    DWORD dw;
+    HANDLE h[2];
+
 	/* Determines whether the process is running in the specified job. 
 	 * ProcessHandle [in]
 	 *   A handle to the process to be tested. 
@@ -414,10 +434,6 @@ int StartRestrictedProcess()
 	}
 	/* Create a job kernel object. */
     hjob = CreateJobObject(NULL, TEXT("Wintellect_RestrictedProcessJob"));
-    /* Place some restrictions on processes in the job. 
-	 * First, set some basic restrictions.
-	 */
-	JOBOBJECT_BASIC_LIMIT_INFORMATION jobli = { 0 };
 	/* The process always runs in the idle priority class. */
 	jobli.PriorityClass = IDLE_PRIORITY_CLASS;
 	/* The job cannot use more than 1 second of CPU time. */
@@ -431,8 +447,6 @@ int StartRestrictedProcess()
 		JobObjectBasicLimitInformation,
 	    &jobli,
 		sizeof(jobli));
-    /* Second, set some UI restrictions. */
-	JOBOBJECT_BASIC_UI_RESTRICTIONS jobuir;
 	/* A fancy zero */
 	jobuir.UIRestrictionsClass = JOB_OBJECT_UILIMIT_NONE;
 	/* The process can't log off the system. */
@@ -455,7 +469,58 @@ int StartRestrictedProcess()
 	 *   suspended so that it can't so that it can't execute any code 
 	 *   outside of the job's restrictions.
 	 */
-	STARTUPINFO si = { sizeof(si) };
-	PROCESS_INFORMATION pi;
+	_tcscpy_s(szCmdLine, _countof(szCmdLine), TEXT("CMD"));
+	bResult = 
+		CreateProcess(
+		   NULL,
+		   szCmdLine,
+		   NULL,
+		   NULL,
+		   FALSE,
+		   CREATE_SUSPENDED | CREATE_NEW_CONSOLE, 
+		   NULL,
+		   NULL,
+		   &si,
+		   &pi);
+	/* Place the process in the job.
+	 * Note: If this process spawns any children, the children
+	 * are automatically part of the same job.
+	 */
+	AssignProcessToJobObject(hjob, pi.hProcess);
+	/* Now we can allow the child process' thread to execute code */
+	ResumeThread(pi.hThread);
+	/* Close handle */
+	CloseHandle(pi.hThread);
+	/* Wailt for the process to terminate or
+	 * for all the job's CPU time to be used.
+	 */
+	h[0] = pi.hProcess;
+    h[1] = hjob;
 
+    dw = WaitForMultipleObjects(2, h, FALSE, INFINITE);
+	switch (dw - WAIT_OBJECT_0)
+	{
+	case 0:
+		printf("The process has terminated...");
+		break;
+	case 1:
+        printf("All of the job's allotted CPU time was used...");
+		break;
+	}
+
+	/* Get timing information about the processes */
+	GetProcessTimes(
+		pi.hProcess,
+		&CreationTime,
+		&ExitTime,
+		&KernelTime,
+		&UserTime);
+	StringCchPrintf(
+		szInfo,
+		_countof(szInfo),
+		TEXT("Kernel = %u | User = %u\n"),
+		KernelTime.dwLowDateTime / 10000,
+		UserTime.dwLowDateTime / 10000);
+	CloseHandle(pi.hProcess);
+	CloseHandle(hjob);
 }
