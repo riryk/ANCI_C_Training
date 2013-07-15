093c1ed2fe9933076f1ae91a735d126b5ff16d86
unknown <Ruslan_Iryk@EPUAKYIW0246.kyiv.epam.com>
2013-07-15 21:04:53 +0300

    Continue socket writing

--- src/include/network/winsocket.h
+++ src/include/network/winsocket.h
@@ -2,6 +2,7 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <stdio.h>
+#include <Nb30.h>
 
 #ifndef WINSOCKET_H
 #define WINSOCKET_H
@@ -11,5 +12,19 @@ void BindSocket();
 int TcpServer();
 void UniversalServerIP6();
 void UniversalServerIP61();
+void IrServer();
+
+// LANANUM and LOCALNAME should be set as in your system
+// For LANA num you may want to use LANACFG.EXE tool
+#define LANANUM     3
+// Padded up to 16 bytes
+#define LOCALNAME  "TERGEDIK        "
+ 
+void NBCheck(NCB x);
+void MakeNetbiosName (char *achDest, LPCWSTR szSrc);
+BOOL NBAddName (int nLana, LPCWSTR szName);
+BOOL NBReset (int nLana, int nSessions, int nNames);
+BOOL NBListNames (int nLana, LPCWSTR szName);
+BOOL NBAdapterStatus (int nLana, PVOID pBuffer, int cbBuffer, LPCWSTR szName);
 
 #endif
