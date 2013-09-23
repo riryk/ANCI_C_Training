e23e968fe03b3c1f08eedf8aebbf06fa91bcc3c3
unknown <Ruslan_Iryk@EPUAKYIW0246.kyiv.epam.com>
2013-09-23 18:45:02 +0300

    Started problem GCJ 2013 3

--- src/include/network/winsockmulticast_IP_TTL.h
+++ src/include/network/winsockmulticast_IP_TTL.h
@@ -10,3 +10,5 @@ void main_multicast(BOOL bReceive);
 void main_multicast_receive();
 void main_multicast_send();
 int main_address_list();
+int main_addr_list_query(int argc, char **argv);
+int main_get_atm_address(int argc, char **argv);
